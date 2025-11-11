0ef00e7ee22f0dc099d95d71f7ffa05ad508c0a8
Dener De Souza Lemos <ddesouza@LPO-169293.local>
2025-11-11 15:54:33 -0500

    submitter

--- runep.sh
+++ runep.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-# Limit ROOT and BLAS threading
-#export ROOT_TTHREADS=1
-#export OMP_NUM_THREADS=1
-#export MKL_NUM_THREADS=1
-#export OPENBLAS_NUM_THREADS=1
-#export VECLIB_MAXIMUM_THREADS=1
-#export NUMEXPR_NUM_THREADS=1
+q_ranges=("q1to10" "q10to100") 
+bools=("false" "true")  # boolean values
 
-q_ranges=("q1to10" "q10to100")
+# Set permissions once
+mkdir -p log results
+chmod 777 log results
 
 for qrange in "${q_ranges[@]}"; do
-  mkdir -p "./log/${qrange}"   # create log dir for this qrange
+  mkdir -p "./log/${qrange}"   
   mkdir -p "./results/${qrange}"
-  chmod 777 "log"
-  chmod 777 "results"
+  
   for i in $(seq -w 0 9); do
-    input_file="inputfiles/ep_10x100_${qrange}/split/part_${i}.list"
-    res_dir="./results/${qrange}"
-    output_file="${res_dir}/JetOut_ep_${qrange}_${i}.histos.root"
-    log_dir="./log/${qrange}"
-    log_file="${log_dir}/out_ep_${qrange}_${i}.log"
-    
-    echo "Running ROOT job for $qrange part $i ..."
-    root -l -q "src/JetTrees.C(\"$input_file\",\"$output_file\")" &> "$log_file"
+    for b1 in "${bools[@]}"; do
+      for b2 in "${bools[@]}"; do
+        input_file="inputfiles/ep_10x100_${qrange}/split/part_${i}.list"
+        output_file="./results/${qrange}/JetOut_ep_${qrange}_${i}_b1_${b1}_b2_${b2}.histos.root"
+        log_file="./log/${qrange}/out_ep_${qrange}_${i}_b1_${b1}_b2_${b2}.log"
+
+        echo "Running ROOT job for $qrange part $i with booleans $b1, $b2 ..."
+        root -l -q "src/JetTreesRecluster.C(\"$input_file\",\"$output_file\",$b1,$b2)" &> "$log_file" &
+      done
+    done
   done
 done
 
