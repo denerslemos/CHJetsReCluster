cf114fd3fd0d687d369f7254fca5b62d91410f44
Dener De Souza Lemos <ddesouza@LPO-169293.local>
2026-01-20 11:48:32 -0500

    add event kinematics

--- src/JetTreesRecluster.C
+++ src/JetTreesRecluster.C
@@ -201,7 +201,6 @@ void JetTreesRecluster(TString InputFileList, TString OutputFile, std::vector<fl
             TVector3 mom((*TrkRecoPx)[i], (*TrkRecoPy)[i], (*TrkRecoPz)[i]);
             if ( mom.Pt() < minCstPt || mom.Pt() > maxCstPt ) continue;
             if ( nhitcut != 0 && (*TrkRecoNhits)[i] < nhitcut ) continue;
-            /*
             if ( removeelectrons == 1 ){
 				// Find electron
 			    int chargePartIndex = i; 
@@ -217,7 +216,6 @@ void JetTreesRecluster(TString InputFileList, TString OutputFile, std::vector<fl
 		      	}
 				if((*TrkMCGenPDG)[elecIndex] == 11) continue;
             }
-            */
             if ( removeelectrons == 2 && i == ScatteredERecId ) continue; 
             PseudoJet p((*TrkRecoPx)[i], (*TrkRecoPy)[i], (*TrkRecoPz)[i], (*TrkRecoE)[i]);
             p.set_user_index(i);
@@ -308,7 +306,6 @@ void JetTreesRecluster(TString InputFileList, TString OutputFile, std::vector<fl
 			    	int chargePartIndex = idx; 
 			    	int elecIndex = -1;
 			    	float elecIndexWeight = -1.0;
-			    	/*
 		    		for(unsigned int itrkass = 0; itrkass < TrkPartAssocRec->GetSize(); itrkass++){ // Loop Over All ReconstructedChargedParticleAssociations
 						if((*TrkPartAssocRec)[itrkass] == chargePartIndex){ // Select Entry Matching the ReconstructedChargedParticle Index
 						    if((*TrkPartAssocWeight)[itrkass] > elecIndexWeight){ // Find Particle with Greatest Weight = Contributed Most Hits to Track
@@ -318,7 +315,6 @@ void JetTreesRecluster(TString InputFileList, TString OutputFile, std::vector<fl
 			  			}
 		      		}
 					if((*TrkMCGenPDG)[elecIndex] == 11) hasElectron = true;
-					*/
                 }
 
                 RecoJet_constituent_pt.push_back(cpt);
